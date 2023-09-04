bba51c8dcd780a195637cda3d2e113d576257f72
Virquan Harold <startupminer@gmail.com>
2023-09-04 18:14:05 -0400

    displaying data structure

--- TreeStructures/App.js
+++ TreeStructures/App.js
@@ -1,11 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
+
+// Data
+const tree = {
+  children: [
+    { name: "node_modules", children: [{ name: "babel" }] },
+    { name: "assets", children: [{ name: "images" }] },
+    { name: "src", children: [{ name: "app" }] },
+  ],
+};
+
+function Entry({ name, children }) {
+  return (
+    <>
+      <Text>{name}</Text>
+      {children?.map((entry) => (
+        <Entry {...entry} />
+      ))}
+    </>
+  );
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Tree Structure</Text>
+      <Text>{tree.children[0].name}</Text>
+      <>
+        {tree.children.map((entry) => {
+          <Entry {...entry} />;
+        })}
+      </>
     </View>
   );
 }
@@ -13,8 +37,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
